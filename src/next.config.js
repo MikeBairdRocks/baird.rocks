609306da89fff82f37dc4a50d01b14c496e3c7e1
Michael Baird <mike@baird.rocks>
2021-04-25 10:38:36 -0500

    webpack5

--- src/next.config.js
+++ src/next.config.js
@@ -4,6 +4,9 @@ const withPWA = require('next-pwa');
 const runtimeCaching = require('next-pwa/cache');
 
 const nextConfig = {
+  future: {
+    webpack5: true
+  },
   pwa: {
     disable: process.env.NODE_ENV === 'development',
     register: true,
