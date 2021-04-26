73fe011ac429248c2270e0ae5e60c20c391aea3c
Michael Baird <mike@baird.rocks>
2021-04-26 08:30:55 -0500

    added cloudflare script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,9 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <script defer src="https://static.cloudflareinsights.com/beacon.min.js" data-cf-beacon='{"token": "08a32db1f41b465e9780f250c2eacb60"}'></script>
+        </Head>
         <body>
           <Main />
           <NextScript />
